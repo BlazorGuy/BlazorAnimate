c0f847a5f76a2d1aeab2ae49f7a54c501bc11d3e
blazorguy <blazorguy@gmail.com>
2020-06-14 21:55:38 -0400

    Initial check-in of module BlazorAnimate

---
+++ BlazorAnimate/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using BlazorAnimate.Services;
+
+namespace BlazorAnimate
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new BezierCurveService());
+            builder.Services.AddTransient(sp => new WordService());
+            builder.Services.AddTransient(sp => new AnimationService(builder.Services.BuildServiceProvider()));
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
